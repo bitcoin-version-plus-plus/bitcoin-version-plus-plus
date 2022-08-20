81b4c7d51aa2806af37ba6df8cca87a99d3ab44e
simewu <swuthier@uccs.edu>
2022-08-20 17:57:52 -0600

    Fix debug username

--- run_debug.sh
+++ run_debug.sh
@@ -1,1 +1,1 @@
-gdb --args src/bitcoind -prune=550 -datadir="/home/ubuntu1/.bitcoin" -debug=handshakeproof
+gdb --args src/bitcoind -prune=550 -datadir="/home/${USER}/.bitcoin" -debug=handshakeproof
