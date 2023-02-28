90f059cd0ea64deca39958bdb8053869a10d8d82
Simeon Wuthier <swuthier@uccs.edu>
2023-02-28 16:05:23 -0700

    Update and rename getBitcoinVerackPlusPlus.sh to getBitcoinVersionPlusPlus.sh

--- getBitcoinVersionPlusPlus.sh
+++ getBitcoinVersionPlusPlus.sh
@@ -1,8 +1,8 @@
 # Copy this script to the desktop before running
 if [[ "$PWD" = "/home/${USER}/Desktop" ]]; then
-    rm -rf bitcoin-verack-plus-plus
-    git clone https://github.com/bitcoin-verack-plus-plus/bitcoin-verack-plus-plus.git
-    cd bitcoin-verack-plus-plus
+    rm -rf bitcoin-version-plus-plus
+    git clone https://github.com/bitcoin-version-plus-plus/bitcoin-version-plus-plus.git
+    cd bitcoin-version-plus-plus
     cp /media/sf_Shared_Folder/minisketch/ src/ -r
     ./first_compile.sh
 else
